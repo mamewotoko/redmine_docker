2a0bd542efd534e091de16af1cdb557921ad1a0c
Takashi Masuyama <mamewotoko@gmail.com>
2017-05-13 20:00:11 +0900

    works

---
+++ library/init.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+mkdir volume/mariadb volume/redmine/plugins volume/redmine/files volume/redmine/log
--- library/volume/redmine/plugins/install.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-bundle exec rake acts_as_taggable_on_engine:install:migrations RAILS_ENV=production
-bundle exec rake db:migrate RAILS_ENV=production
-bundle exec rake db:migrate_plugins RAILS_ENV=production
